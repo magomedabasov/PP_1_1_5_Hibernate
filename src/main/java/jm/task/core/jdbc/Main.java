c5803d4194c5d94be2ae5d9c30b70df22e3c9231
sebeb-siz <sebebsiz93@gmail.com>
2023-05-25 00:14:47 +0300

    UserService вместо UserServiceImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl usi = new UserServiceImpl();
-        usi.createUsersTable();
-        usi.saveUser("Harry", "LastName0", (byte) 51);
-        usi.saveUser("Tom", "LastName1", (byte) 20);
-        usi.saveUser("Bill", "LastName2", (byte) 25);
-        usi.saveUser("Bob", "LastName3", (byte) 31);
-        usi.getAllUsers().forEach(System.out::println);
-        usi.cleanUsersTable();
-        usi.dropUsersTable();
+        UserService uService = new UserServiceImpl();
+        uService.createUsersTable();
+        uService.saveUser("Harry", "LastName0", (byte) 51);
+        uService.saveUser("Tom", "LastName1", (byte) 20);
+        uService.saveUser("Bill", "LastName2", (byte) 25);
+        uService.saveUser("Bob", "LastName3", (byte) 31);
+        uService.getAllUsers().forEach(System.out::println);
+        uService.cleanUsersTable();
+        uService.dropUsersTable();
         }
     }
