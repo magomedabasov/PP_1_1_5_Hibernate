7d40c9e509de82cfe611c58a61b1c34fdca90750
sebeb-siz <sebebsiz93@gmail.com>
2023-05-25 00:06:48 +0300

    Логика соединения в Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,16 +1,23 @@
 package jm.task.core.jdbc.util;
 
-import com.mysql.cj.log.Log;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String DATABASE = "mydbtest";
-    public static final String URL = "jdbc:mysql://localhost:3306/" + DATABASE;
-    public static final String USER = "root";
-    public static final String PASSWORD = "root";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
